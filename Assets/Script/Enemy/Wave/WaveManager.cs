12ebae30631708e40af68c4fe1601894d447fbd4
Xezy12 <wasurath2007@gmail.com>
2024-08-10 16:21:19 +0700

    StartNewWaves

--- Assets/Script/Enemy/Wave/WaveManager.cs
+++ Assets/Script/Enemy/Wave/WaveManager.cs
@@ -4,6 +4,7 @@ public class WaveManager : MonoBehaviour
 {
     public int currentWave = 1;
     public int enemiesPerWave = 5;
+    private int enemiesAlive = 0;
     private Spawner spawner;
 
     private void Start()
@@ -12,18 +13,39 @@ public class WaveManager : MonoBehaviour
         StartWave();
     }
 
+    private void Update()
+    {
+        // Check if all enemies are killed
+        if (enemiesAlive == 0)
+        {
+            StartNextWave();
+        }
+    }
+
     public void StartWave()
     {
-        int maxEnemies = enemiesPerWave * currentWave;
-        for (int i = 0; i < maxEnemies; i++)
+        enemiesAlive = enemiesPerWave * currentWave;
+
+        for (int i = 0; i < enemiesAlive; i++)
         {
             spawner.SpawnEnemy();
         }
     }
 
-    public void IncreaseDifficulty()
+    public void EnemyKilled()
+    {
+        enemiesAlive--;
+    }
+
+    private void StartNextWave()
     {
         currentWave++;
+        IncreaseDifficulty();
+        StartWave();
+    }
+
+    public void IncreaseDifficulty()
+    {
         float multiplier = 1f + (currentWave * 0.1f);
 
         // Adjust enemy stats based on the wave
