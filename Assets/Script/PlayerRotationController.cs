28795afa64039fcd930d1a88fdbd3912a068e255
Vinniel5403 <vinniel.gawintep@gmail.com>
2024-08-10 14:51:12 +0700

    PlayerSprite

---
+++ Assets/Script/PlayerRotationController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LookAtCursor : MonoBehaviour
+{
+    public float rotationSpeed = 5f;
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+        );
+
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+
+        float currentAngle = transform.rotation.eulerAngles.z;
+        
+        float angle = Mathf.LerpAngle(currentAngle, targetAngle, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
