8e98548eedf0e1dafd810a97fb0254eb3e4aee29
Xezy12 <wasurath2007@gmail.com>
2024-08-10 15:47:57 +0700

    Enemy

---
+++ Assets/Script/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public float health;
+    public float damage;
+    public float speed;
+    protected IAttackPattern attackPattern;
+
+    protected virtual void Start()
+    {
+        // Initialize with default stats or override in subclasses
+    }
+
+    public virtual void Attack()
+    {
+        attackPattern.ExecuteAttack(this);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected void Die()
+    {
+        // Handle enemy death, e.g., play animation, remove from game
+        Destroy(gameObject);
+    }
+}
