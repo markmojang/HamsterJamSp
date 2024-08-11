3149289c227084bce52899f423591844311065a7
markmojang <markmojang555@gmail.com>
2024-08-11 19:47:41 +0700

    Go bro

---
+++ Assets/Script/SetValuePlayerPrefs.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetValuePlayerPrefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("")){
+
+        }
+        if(PlayerPrefs.HasKey("")){
+            
+        }
+        if(PlayerPrefs.HasKey("")){
+            
+        }
+        if(PlayerPrefs.HasKey("")){
+            
+        }
+        if(PlayerPrefs.HasKey("")){
+            
+        }
+    }
+
+}
