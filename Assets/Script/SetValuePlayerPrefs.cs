233de9da15e416250905d91d4986418557c81143
Vinniel5403 <vinniel.gawintep@gmail.com>
2024-08-11 20:47:14 +0700

    BalanceChange

--- Assets/Script/SetValuePlayerPrefs.cs
+++ Assets/Script/SetValuePlayerPrefs.cs
@@ -8,7 +8,7 @@ public class SetValuePlayerPrefs : MonoBehaviour
     void Start()
     {
         if(!PlayerPrefs.HasKey("PmaxHp")){
-            PlayerPrefs.SetFloat("PmaxHp", 200f);
+            PlayerPrefs.SetFloat("PmaxHp", 100f);
             PlayerPrefs.Save();
         }
         if(!PlayerPrefs.HasKey("PDamage")){
